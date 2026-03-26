ff8ab93ae7b13177a9327e764c12d41ebb315627
Martin <martin-talle@hotmail.com>
2026-03-26 19:45:58 +0000

    Revert "Allow running LagerPro without database"
    This reverts commit baddcf71806adededb2d45da630c50d0dd441caa.

--- src/LagerPro.Api/Program.cs
+++ src/LagerPro.Api/Program.cs
@@ -6,13 +6,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddApplication();
+builder.Services.AddInfrastructure(builder.Configuration);
 builder.Services.AddControllers();
-
-var useDatabase = builder.Configuration.GetValue<bool>("UseDatabase", false);
-if (useDatabase)
-{
-    builder.Services.AddInfrastructure(builder.Configuration);
-}
 
 var app = builder.Build();
 
@@ -24,6 +19,6 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 app.MapControllers();
-app.MapGet("/health", () => Results.Ok(new { status = "ok", database = useDatabase ? "enabled" : "disabled" }));
+app.MapGet("/health", () => Results.Ok(new { status = "ok" }));
 
 app.Run();
