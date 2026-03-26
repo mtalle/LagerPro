d017d76478b3702ef24962f45a36aa2028dab5ed
Martin <martin-talle@hotmail.com>
2026-03-26 19:45:58 +0000

    Revert "Fix in-memory article mapping"
    This reverts commit 1f070d9084f9111bb8a1d63f0f3f9b65a8331003.

--- src/LagerPro.Application/Features/Articles/Queries/GetAllArticles/GetAllArticlesHandler.cs
+++ src/LagerPro.Application/Features/Articles/Queries/GetAllArticles/GetAllArticlesHandler.cs
@@ -1,20 +1,20 @@
-using LagerPro.Application.Abstractions;
-using LagerPro.Contracts.Dtos.Articles;
+using LagerPro.Domain.Repositories;
 
 namespace LagerPro.Application.Features.Articles.Queries.GetAllArticles;
 
 public class GetAllArticlesHandler
 {
-    private readonly IAppReadStore _store;
+    private readonly IArtikkelRepository _repository;
 
-    public GetAllArticlesHandler(IAppReadStore store)
+    public GetAllArticlesHandler(IArtikkelRepository repository)
     {
-        _store = store;
+        _repository = repository;
     }
 
-    public Task<IReadOnlyList<ArticleDto>> Handle(CancellationToken cancellationToken = default)
+    public async Task<IReadOnlyList<ArticleDto>> Handle(CancellationToken cancellationToken = default)
     {
-        var articles = _store.Artikler.Select(x => new ArticleDto(
+        var articles = await _repository.GetAllAsync(cancellationToken);
+        return articles.Select(x => new ArticleDto(
             x.Id,
             x.ArtikkelNr,
             x.Navn,
@@ -27,7 +27,5 @@ public class GetAllArticlesHandler
             x.Utpris,
             x.MinBeholdning,
             x.Aktiv)).ToList();
-
-        return Task.FromResult<IReadOnlyList<ArticleDto>>(articles);
     }
 }
