afa1534dbfa24e586fca874c0df0f70b6dcaac97
Martin <martin-talle@hotmail.com>
2026-03-26 19:45:58 +0000

    Revert "Fix in-memory store build issues"
    This reverts commit 5d5e106b8626fbb19302897ee5f4a1c4b0f299af.

--- src/LagerPro.Application/Features/Articles/Commands/CreateArticle/CreateArticleHandler.cs
+++ src/LagerPro.Application/Features/Articles/Commands/CreateArticle/CreateArticleHandler.cs
@@ -1,16 +1,19 @@
 using LagerPro.Application.Abstractions;
 using LagerPro.Domain.Entities;
 using LagerPro.Domain.Enums;
+using LagerPro.Domain.Repositories;
 
 namespace LagerPro.Application.Features.Articles.Commands.CreateArticle;
 
 public class CreateArticleHandler
 {
-    private readonly IAppWriteStore _store;
+    private readonly IArtikkelRepository _repository;
+    private readonly IUnitOfWork _unitOfWork;
 
-    public CreateArticleHandler(IAppWriteStore store)
+    public CreateArticleHandler(IArtikkelRepository repository, IUnitOfWork unitOfWork)
     {
-        _store = store;
+        _repository = repository;
+        _unitOfWork = unitOfWork;
     }
 
     public async Task<int> Handle(CreateArticleCommand command, CancellationToken cancellationToken = default)
@@ -30,7 +33,9 @@ public class CreateArticleHandler
             Aktiv = true
         };
 
-        await _store.AddArtikkelAsync(article, cancellationToken);
-        return 0;
+        await _repository.AddAsync(article, cancellationToken);
+        await _unitOfWork.SaveChangesAsync(cancellationToken);
+
+        return article.Id;
     }
 }
