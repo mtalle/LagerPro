11a076f1208a35e307512a228159b27621e71087
Martin <martin-talle@hotmail.com>
2026-03-26 19:33:21 +0000

    Remove unresolved customer and supplier handlers from startup

--- src/LagerPro.Application/DependencyInjection/ServiceCollectionExtensions.cs
+++ src/LagerPro.Application/DependencyInjection/ServiceCollectionExtensions.cs
@@ -1,5 +1,5 @@
-using LagerPro.Application.Features.Kunder;
-using LagerPro.Application.Features.Leverandorer;
+using LagerPro.Application.Features.Articles.Commands.CreateArticle;
+using LagerPro.Application.Features.Articles.Queries.GetAllArticles;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace LagerPro.Application.DependencyInjection;
@@ -8,8 +8,8 @@ public static class ServiceCollectionExtensions
 {
     public static IServiceCollection AddApplication(this IServiceCollection services)
     {
-        services.AddScoped<CreateKundeHandler>();
-        services.AddScoped<CreateLeverandorHandler>();
+        services.AddScoped<GetAllArticlesHandler>();
+        services.AddScoped<CreateArticleHandler>();
         return services;
     }
 }
