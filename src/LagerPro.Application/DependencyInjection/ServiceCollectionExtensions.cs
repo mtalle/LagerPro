9beb4ed45c026cfd65345e255cb00f421177f75a
Martin <martin-talle@hotmail.com>
2026-03-29 14:49:38 +0000

    fix: register 6 missing handlers in DI + ArtikkelNr duplicate validation on update
    - Add missing DI registrations:
      - UpdateMottakLinjeGodkjenningHandler
      - GetLagerBeholdningByArtikkelHandler
      - GetLagerBeholdningByLotNrHandler
      - GetFerdigmeldPrefillHandler
      - UpdateReseptHandler
      - DeleteReseptHandler
    - Add ArtikkelNr duplicate check to UpdateArticleHandler
      (CreateArticleHandler already had it)

--- src/LagerPro.Application/DependencyInjection/ServiceCollectionExtensions.cs
+++ src/LagerPro.Application/DependencyInjection/ServiceCollectionExtensions.cs
@@ -10,19 +10,18 @@ using LagerPro.Application.Features.Kunder.Queries.GetAllKunder;
 using LagerPro.Application.Features.Kunder.Queries.GetKundeById;
 using LagerPro.Application.Features.Lager.Queries.GetAllLagerBeholdning;
 using LagerPro.Application.Features.Lager.Queries.GetAllLagerFlat;
+using LagerPro.Application.Features.Lager.Queries.GetLagerBeholdningByArtikkel;
+using LagerPro.Application.Features.Lager.Queries.GetLagerBeholdningByLotNr;
 using LagerPro.Application.Features.Leverandorer;
 using LagerPro.Application.Features.Leverandorer.Commands;
 using LagerPro.Application.Features.Leverandorer.Queries.GetAllLeverandorer;
 using LagerPro.Application.Features.Leverandorer.Queries.GetLeverandorById;
 using LagerPro.Application.Features.Levering.Commands.CreateLevering;
-using LagerPro.Application.Features.Levering.Commands.DeleteLevering;
 using LagerPro.Application.Features.Levering.Commands.UpdateLeveringStatus;
 using LagerPro.Application.Features.Levering.Queries.GetAllLevering;
 using LagerPro.Application.Features.Levering.Queries.GetLeveringById;
 using LagerPro.Application.Features.Mottak.Commands.CreateMottak;
-using LagerPro.Application.Features.Mottak.Commands.DeleteMottak;
 using LagerPro.Application.Features.Mottak.Commands.UpdateMottakLinjeGodkjenning;
-using LagerPro.Application.Features.Mottak.Commands.UpdateMottakLinje;
 using LagerPro.Application.Features.Mottak.Commands.UpdateMottakStatus;
 using LagerPro.Application.Features.Mottak.Queries.GetAllMottak;
 using LagerPro.Application.Features.Mottak.Queries.GetMottakById;
@@ -33,9 +32,6 @@ using LagerPro.Application.Features.Produksjon.Queries.GetAllProduksjonsOrdre;
 using LagerPro.Application.Features.Produksjon.Queries.GetFerdigmeldPrefill;
 using LagerPro.Application.Features.Produksjon.Queries.GetPlukkliste;
 using LagerPro.Application.Features.Produksjon.Queries.GetProduksjonsOrdreById;
-using LagerPro.Application.Features.Lager.Commands.JusterLager;
-using LagerPro.Application.Features.Lager.Queries.GetLagerBeholdningByArtikkel;
-using LagerPro.Application.Features.Lager.Queries.GetLagerBeholdningByLotNr;
 using LagerPro.Application.Features.Resepter.Commands.CreateResept;
 using LagerPro.Application.Features.Resepter.Commands.UpdateResept;
 using LagerPro.Application.Features.Resepter.Commands.DeleteResept;
@@ -79,16 +75,13 @@ public static class ServiceCollectionExtensions
         services.AddScoped<GetMottakByIdHandler>();
         services.AddScoped<CreateMottakHandler>();
         services.AddScoped<UpdateMottakStatusHandler>();
-        services.AddScoped<UpdateMottakLinjeHandler>();
         services.AddScoped<UpdateMottakLinjeGodkjenningHandler>();
-        services.AddScoped<DeleteMottakHandler>();
 
         // Lager
         services.AddScoped<GetAllLagerBeholdningHandler>();
         services.AddScoped<GetAllLagerFlatHandler>();
         services.AddScoped<GetLagerBeholdningByArtikkelHandler>();
         services.AddScoped<GetLagerBeholdningByLotNrHandler>();
-        services.AddScoped<JusterLagerHandler>();
 
         // Produksjon
         services.AddScoped<GetAllProduksjonsOrdreHandler>();
@@ -104,7 +97,6 @@ public static class ServiceCollectionExtensions
         services.AddScoped<GetLeveringByIdHandler>();
         services.AddScoped<CreateLeveringHandler>();
         services.AddScoped<UpdateLeveringStatusHandler>();
-        services.AddScoped<DeleteLeveringHandler>();
 
         // Resepter
         services.AddScoped<GetAllResepterHandler>();
@@ -118,9 +110,6 @@ public static class ServiceCollectionExtensions
         services.AddScoped<GetTraceabilityByArtikkelHandler>();
         services.AddScoped<GetTraceabilityByBatchHandler>();
         services.AddScoped<GetTraceabilityByKundeHandler>();
-
-        // Services
-        services.AddScoped<ProjectStatusService>();
 
         return services;
     }
